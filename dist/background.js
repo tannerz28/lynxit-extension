8ed3fb7419f57644be8f184549960528aa2618b8
tgoins <tgoins@iwtsolutions.com>
2017-07-19 13:36:45 -0500

    notifications

---
+++ dist/background.js
@@ -0,0 +1,24 @@
+const INTERVAL = 1000 * 30;
+
+chrome.notifications.onClicked.addListener(function(notificationId) {
+	if(notificationId === 'testNotification') {
+		chrome.tabs.create({ url: 'http://qa.lynxithealth.com' });
+		chrome.notifications.clear(notificationId);
+	}
+});
+
+const showNotification = function() {
+	chrome.notifications.create('testNotification', {
+		type: 'basic',
+		title: 'You\'ve got mail',
+		message: 'You have received a page.',
+		contextMessage: 'Lynxit',
+		iconUrl: 'http://qa.lynxithealth.com/images/LynxIT_LOGO_50x183.png'
+	});
+};
+
+if(window.Notification) {
+	setInterval(function () {
+		//
+	}, INTERVAL);
+}
